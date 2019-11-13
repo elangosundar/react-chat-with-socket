140e7ac422826d857128bb7cc08b98774fc014e3
myungwoo-Y <auddn6676@naver.com>
2019-11-14 05:34:54 +0900

    add input

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './InfoBar.css';
 
-import closeIcon from '../../icons/closeIcon'
-import onlineIcon from '../../icons/onlineIcon'
+import closeIcon from '../../icons/closeIcon.png'
+import onlineIcon from '../../icons/onlineIcon.png'
 
 
-const InfoBar = ({ room }) => {
+const InfoBar = ({ room }) => (
     <div className="infoBar">
         <div className="leftInnerContainer">
             <img className="onlineIcon" src={onlineIcon}/>
@@ -15,7 +15,7 @@ const InfoBar = ({ room }) => {
             <a href="/"><img src={closeIcon} alt="close image"/></a>
         </div>
     </div>
-}
+)
 
 export default InfoBar;
 
