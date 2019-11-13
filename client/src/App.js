88528e2f09be3710b9c3e07fbb747a0ca6787d87
myungwoo-Y <auddn6676@naver.com>
2019-11-13 19:56:53 +0900

    add basic component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import {BrowerRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Join from './components/Join';
 import Chat from './components/Chat';
 
-const App = () => {
+const App = () => (
     <Router>
         <Route path="/" exact component={Join}/>
         <Route path="/chat" exact component={Chat}/>
     </Router>
-}
+)
 
 export default App;
