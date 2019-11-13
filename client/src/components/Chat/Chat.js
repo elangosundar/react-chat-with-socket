043674cf02ef81b75a662729fcc0f8b7433e2f15
myungwoo-Y <auddn6676@naver.com>
2019-11-14 04:05:10 +0900

    connect socket in client

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+    useEffect(() => {
+        // location : url으로서 router가 전달해준다.
+        const { name, room } = queryString.parse(location.search);
+        
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+        // 1번만 렌더되기 위해 사용
+    }, [ENDPOINT, location.search])
     return(
         <h1>Chat</h1>
     )
