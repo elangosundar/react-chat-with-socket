de9b9c99379e75f4b78ed1d8f91e587e5688abf0
myungwoo-Y <auddn6676@naver.com>
2019-11-14 03:52:50 +0900

    fix hook in join.js

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import './Join.css'
+
 const Join = () => {
-    const {name, setName} = useState('');
-    const {room, setRoom} = useState('');
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
 
     return(
         <div className="joinOuterContainer">
