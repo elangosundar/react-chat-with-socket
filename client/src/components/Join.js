08eb477babc555df7113602a0c05aa5f8c3cd0d3
myungwoo-Y <auddn6676@naver.com>
2019-11-14 03:40:19 +0900

    add input tag in join

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const {name, setName} = useState('');
+    const {room, setRoom} = useState('');
+
     return(
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
+                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}/></div>
+                {/* name 과 room 값이 비어있는 경우 방지 */}
+                <Link onClick={evnet => ( !name || !room) ? evnet.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button" type="submit">Sign in</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
